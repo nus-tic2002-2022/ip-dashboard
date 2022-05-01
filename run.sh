ed258f2cb443099fec9b143ee7a386d056bf0695
Damith C. Rajapakse <damith@gmail.com>
2022-05-01 14:43:07 +0800

    Update end date

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 ./get-reposense.py --master
-java -jar RepoSense.jar --config ./configs --until 21/03/2022 --since 10/01/2022  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
+java -jar RepoSense.jar --config ./configs --until 01/05/2022 --since 10/01/2022  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
